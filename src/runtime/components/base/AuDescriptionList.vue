3b9e91d807cdb27c53d65d38c192db628a6e3722
Sam K <33263129+samk-dev@users.noreply.github.com>
2023-06-23 23:07:21 +0200

    feat: description list component

---
+++ src/runtime/components/base/AuDescriptionList.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'AuDescriptionList'
+})
+
+interface PropsAuDescriptionList {
+  /**
+   * @description horizontal line between list item
+   * @default true
+   */
+  divider?: boolean
+  /**
+   * @description description list items
+   */
+  items: any
+}
+const props = withDefaults(defineProps<PropsAuDescriptionList>(), {
+  divider: true
+})
+</script>
+
+<template>
+  <dl
+    :class="[
+      'au-description-list',
+      props.divider ? 'au-description-list-divider' : ''
+    ]"
+  >
+    <template v-for="(item, i) in props.items" :key="i">
+      <dt>
+        {{ item.term }}
+      </dt>
+
+      <dd>
+        {{ item.description }}
+      </dd>
+    </template>
+  </dl>
+</template>
